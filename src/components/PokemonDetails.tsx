0f3908946be3769df02c03a2a92bf85619833f3d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-01 18:24:31 +0530

    feat: setup details component

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,4 +1,6 @@
-import { type FC } from "react";
+import { type FC, useState, useEffect } from "react";
+import { PokeDetailSchema, type TPokeDetail } from "~/types";
+import Image from "next/image";
 
 interface Props {
   id: string;
@@ -6,6 +8,69 @@ interface Props {
 
 export const PokemonDetails: FC<Props> = (props) => {
   const { id } = props;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({
+    isError: false,
+    message: "",
+  });
+  const [details, setDetails] = useState<TPokeDetail>();
 
-  return <>Details: {id}</>;
+  useEffect(() => {
+    /**
+     * Fetches the details of a pokemon
+     */
+    const fetchPokeDetails = async () => {
+      try {
+        setLoading(true);
+
+        if (id) {
+          const data = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${id}`
+          ).then(async (response) =>
+            PokeDetailSchema.parse(await response.json())
+          );
+
+          setDetails(data);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        setError({
+          isError: true,
+          message: "Something went wrong",
+        });
+      }
+    };
+
+    void fetchPokeDetails();
+  }, [id]);
+
+  return (
+    <div>
+      <h1>PokeDetails</h1>
+
+      {!loading && !error.isError && details && (
+        <div>
+          <h2>Name: {details.name}</h2>
+
+          <Image
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+            alt={details.name}
+            width={200}
+            height={200}
+          />
+
+          <h2>Height: {details.height}</h2>
+          <h2>Weight: {details.weight}</h2>
+
+          <h2>Base XP: {details.base_experience}</h2>
+        </div>
+      )}
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && error.isError && <p>{error.message}</p>}
+    </div>
+  );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -13,3 +13,26 @@ export const PokeListSchema = z.object({
 });
 
 export type TPokeList = z.infer<typeof PokeListSchema>;
+
+export const PokeDetailSchema = z.object({
+  abilities: z.array(z.object({})),
+  base_experience: z.number(),
+  forms: z.array(z.object({})),
+  game_indices: z.array(z.object({})),
+  height: z.number(),
+  held_items: z.array(z.object({})),
+  id: z.number(),
+  is_default: z.boolean(),
+  location_area_encounters: z.string(),
+  moves: z.array(z.object({})),
+  name: z.string(),
+  order: z.number(),
+  past_types: z.array(z.object({})),
+  species: z.object({}),
+  sprites: z.object({}),
+  stats: z.array(z.object({})),
+  types: z.array(z.object({})),
+  weight: z.number(),
+});
+
+export type TPokeDetail = z.infer<typeof PokeDetailSchema>;
